3fa80ca00cc24677f0510d401912dbca568f6f00
amit13579 <91464104+amit13579@users.noreply.github.com>
2024-02-27 02:59:25 +0530

    Add files via upload

---
+++ src/main/java/Amit/hotel/services/ReservationService.java
@@ -0,0 +1,21 @@
+package Amit.hotel.services;
+
+import java.util.Collection;
+
+import Amit.hotel.entity.Reservation;
+import Amit.hotel.temp.CurrentReservation;
+
+//Service Pattern for Reservation
+public interface ReservationService {
+	
+	public Reservation getReservationForLoggedUserById(int resId);
+
+	public Collection<Reservation> getReservationsForLoggedUser();
+	
+	public void saveOrUpdateReservation(CurrentReservation currentReservation);
+	
+	public void deleteReservation(int resId);
+
+	public CurrentReservation reservationToCurrentReservationById(int resId);
+
+}
